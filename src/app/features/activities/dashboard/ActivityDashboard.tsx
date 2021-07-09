7319fb343e1010a6310bbbd274426d4bf74dac6f
migarciaduran <69094918+migarciaduran@users.noreply.github.com>
2021-07-08 20:07:53 -0600

    Adding User Login + Register (#11)
    * Adding Login
    
    * Adding Register Form
    
    * Adding Validations

--- src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,7 +15,7 @@ function ActivityDashboard() {
   }, [activityRegistry.size, loadActivities]);
 
   if (activityStore.loadingInitial)
-    return <LoadingComponent content="Loading app" />;
+    return <LoadingComponent content="Loading activites..." />;
 
   return (
     <div>
--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
@@ -12,13 +12,31 @@ import TestErrors from "../features/errors/TestErrors";
 import { ToastContainer } from "react-toastify";
 import NotFound from "../features/errors/NotFound";
 import ServerError from "../stores/ServerError";
+import LoginForm from "../features/users/LoginForm";
+import { useStore } from "../stores/store";
+import LoadingComponent from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
 
 function App() {
   const location = useLocation();
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore]);
+
+  if (!commonStore.appLoaded) {
+    <LoadingComponent content="Loading app..." />;
+  }
 
   return (
     <>
       <ToastContainer position="bottom-right" hideProgressBar />
+      <ModalContainer />
       <Route exact path="/" component={HomePage} />
       <Route
         path={"/(.+)"}
@@ -36,6 +54,7 @@ function App() {
                 />
                 <Route path="/errors" component={TestErrors} />
                 <Route path="/server-error" component={ServerError} />
+                <Route path="/login" component={LoginForm} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
